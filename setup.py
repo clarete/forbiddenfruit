af83940661e56a944c84ed84df4c6dfca2c54c26
Lincoln de Sousa <lincoln@comum.org>
2019-04-20 09:45:02 -0400

    chore: Update copyright info & add MIT as alternative license

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 # forbiddenfruit - Patch built-in python objects
 #
-# Copyright (c) 2013  Lincoln de Sousa <lincoln@comum.org>
+# Copyright (c) 2013,2019  Lincoln de Sousa <lincoln@clarete.li>
 #
+# This program is dual licensed under GPLv3 and MIT.
+#
+# GPLv3
+# -----
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
@@ -14,6 +18,28 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# MIT
+# ---
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+# BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+# ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 
 import os
 from setuptools import setup, find_packages, Extension
@@ -32,8 +58,12 @@ if __name__ == '__main__':
         description='Patch python built-in objects',
         long_description=local_file('README.md'),
         author='Lincoln de Sousa',
-        author_email='lincoln@comum.org',
+        author_email='lincoln@clarete.li',
         url='https://github.com/clarete/forbiddenfruit',
         packages=find_packages(exclude=['*tests*']),
         ext_modules=[ffruit],
+        classifiers=[
+            'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+            'License :: OSI Approved :: MIT License',
+        ],
     )
